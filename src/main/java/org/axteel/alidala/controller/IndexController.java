dffac7b60bacf1a6bb0ce9f98b8356956c2dba7e
AndreySt33l <stalnoyandrey.private@gmail.com>
2020-07-20 15:13:33 +0600

    Anither try

--- src/main/java/org/axteel/alidala/controller/IndexController.java
+++ src/main/java/org/axteel/alidala/controller/IndexController.java
@@ -1,12 +1,15 @@
 package org.axteel.alidala.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-    @RequestMapping (value = "/users")
-    public String getUsers() {
-        return "users";
+    @RequestMapping (value = "/", method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
     }
+
 }
